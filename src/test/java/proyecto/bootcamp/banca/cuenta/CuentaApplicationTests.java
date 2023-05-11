cc3fd4979ee966a8ac98858de8abb7a1a6b2f05c
dnns <dennis.ah@gmail.com>
2023-05-11 16:51:51 -0500

    Proyecto base cuenta

---
+++ src/test/java/proyecto/bootcamp/banca/cuenta/CuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.bootcamp.banca.cuenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
